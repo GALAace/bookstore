6893fbd7372e772838738f793586f5fc6293ed4c
GALA <galaace@qq.com>
2019-05-10 23:13:31 +0800

    project init commit

---
+++ src/main/java/com/bs/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.bs.dao;
+
+import com.bs.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/bs/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.bs.dao;
+
+import com.bs.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
